443cbe3f4e36788f0c536b5e346b265c6b35a100
João Silva <jaasilva@netcabo.pt>
2013-03-05 16:11:09 +0000

    missing annotation

---
+++ src/java/org/deuce/distribution/replication/partial/PartialReplication.java
@@ -0,0 +1,15 @@
+package org.deuce.distribution.replication.partial;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import org.deuce.transform.ExcludeTM;
+
+@Target(FIELD)
+@Retention(CLASS)
+@ExcludeTM
+public @interface PartialReplication
+{
+
+}
