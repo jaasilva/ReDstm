f87eab2111ca99a278e9aba99b8bf2493059e966
jaasilva <jaasilva@netcabo.pt>
2013-03-15 12:40:09 +0000

    test stuff

---
+++ run.sh
@@ -0,0 +1,7 @@
+for node in node1 node2 node3 node4 node5 node6 node7 node8
+do
+	ssh $node
+	cd ./repos/trxsys-tvale
+	java -cp .:bin/:bin/classes:etc:lib/jgroups-3.2.7.Final.jar:lib/log4j-1.2.14.jar -Dtribu.replicas=10 org.deuce.partial.toa.perf.Receiver $node &
+	exit
+done
