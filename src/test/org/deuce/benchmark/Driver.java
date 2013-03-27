fe3dbd005de0f9b882ec4cc968431b7452f6fdf4
jaasilva <jaasilva@netcabo.pt>
2013-03-27 10:53:47 +0000

    prints in benchmark driver

--- src/test/org/deuce/benchmark/Driver.java
+++ src/test/org/deuce/benchmark/Driver.java
@@ -1,5 +1,7 @@
 package org.deuce.benchmark;
 
+import java.util.Scanner;
+
 import org.deuce.Atomic;
 import org.deuce.distribution.TribuDSTM;
 import org.deuce.distribution.replication.Bootstrap;
@@ -96,8 +98,8 @@ public class Driver
 
 		b.init(args);
 
-		// System.err.println("-- Set created. Press enter to start threads.");
-		// new Scanner(System.in).nextLine();
+		 System.err.println("-- Set created. Press enter to start threads.");
+		 new Scanner(System.in).nextLine();
 		initBarriers();
 		setupBarrier.join();
 
@@ -111,14 +113,14 @@ public class Driver
 		for (int i = 0; i < t.length; i++)
 			t[i] = new Thread(bt[i]);
 
-		// System.out.print("Starting threads...");
+		 System.out.print("Starting threads...");
 		for (int i = 0; i < t.length; i++)
 		{
-			// System.out.print(" " + i);
+			 System.out.print(" " + i);
 			bt[i].setPhase(Benchmark.WARMUP_PHASE);
 			t[i].start();
 		}
-		// System.out.println();
+		 System.out.println();
 
 		long wstart = System.currentTimeMillis();
 		try
@@ -130,13 +132,13 @@ public class Driver
 		}
 		long wend = System.currentTimeMillis();
 
-		// System.out.print("End of warmup phase...");
+		 System.out.print("End of warmup phase...");
 		for (int i = 0; i < bt.length; i++)
 		{
-			// System.out.print(" " + i);
+			 System.out.print(" " + i);
 			bt[i].setPhase(Benchmark.TEST_PHASE);
 		}
-		// System.out.println();
+		 System.out.println();
 
 		long tstart = System.currentTimeMillis();
 		try
@@ -148,21 +150,21 @@ public class Driver
 		}
 		long tend = System.currentTimeMillis();
 
-		// System.out.print("End of test phase...");
+		 System.out.print("End of test phase...");
 		for (int i = 0; i < bt.length; i++)
 		{
-			// System.out.print(" " + i);
+			 System.out.print(" " + i);
 			bt[i].setPhase(Benchmark.SHUTDOWN_PHASE);
 		}
-		// System.out.println();
+		 System.out.println();
 
-		// System.out.print("Waiting for threads to finish...");
+		 System.out.print("Waiting for threads to finish...");
 		for (int i = 0; i < t.length; i++)
 		{
 			try
 			{
 				t[i].join();
-				// System.out.print(" " + i);
+				 System.out.print(" " + i);
 			}
 			catch (InterruptedException e)
 			{
@@ -183,15 +185,15 @@ public class Driver
 		for (int i = 0; i < bt.length; i++)
 			System.out.println("    " + i + " : " + bt[i].getSteps() + " ("
 					+ bt[i].getStats() + ")");
-		// System.err.println("-- Benchmark finished. Press enter to exit.");
-		// new Scanner(System.in).nextLine();
+		 System.err.println("-- Benchmark finished. Press enter to exit.");
+		 new Scanner(System.in).nextLine();
 
 		finishBarrier.join();
 		// Profiler.enabled = false;
 
 		// Profiler.print();
 
-		// System.out.println(((org.deuce.benchmark.intset.Benchmark) b).m_set);
+		 System.out.println(((org.deuce.benchmark.intset.Benchmark) b).m_set);
 
 		TribuDSTM.close();
 	}
