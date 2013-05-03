3736cce7cb6b5b431001c1a560a5a202256a3200
jaasilva <jaasilva@netcabo.pt>
2013-05-03 14:06:43 +0100

    added group package (Group interface and PartialReplicationGroup class).

---
+++ src/java/org/deuce/distribution/replication/group/Group.java
@@ -0,0 +1,32 @@
+package org.deuce.distribution.replication.group;
+
+import java.util.Collection;
+
+import org.deuce.distribution.groupcomm.Address;
+
+/**
+ * @author jaasilva
+ * 
+ */
+public interface Group
+{
+	public Collection<Address> getAll();
+
+	public boolean contains(Address addr);
+
+	public boolean add(Address addr);
+
+	public boolean addAll(Collection<Address> addrs);
+
+	public boolean remove(Address addr);
+
+	public int size();
+
+	public String toString();
+
+	public boolean equals(Object obj);
+
+	public int hashCode();
+
+	public Group union(Group other);
+}
---
+++ src/java/org/deuce/distribution/replication/group/PartialReplicationGroup.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package org.deuce.distribution.replication.group;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.deuce.distribution.groupcomm.Address;
+
+/**
+ * @author jaasilva
+ * 
+ */
+public class PartialReplicationGroup implements Group
+{
+	private Set<Address> addrs;
+
+	public PartialReplicationGroup()
+	{
+		this.addrs = new HashSet<Address>();
+	}
+
+	public PartialReplicationGroup(Collection<? extends Address> addrs)
+	{
+		this.addrs = new HashSet<Address>(addrs);
+	}
+
+	@Override
+	public Collection<Address> getAll()
+	{
+		return this.addrs;
+	}
+
+	@Override
+	public boolean contains(Address addr)
+	{
+		return this.addrs.contains(addr);
+	}
+
+	@Override
+	public boolean add(Address addr)
+	{
+		return this.addrs.add(addr);
+	}
+
+	@Override
+	public boolean addAll(Collection<Address> addrs)
+	{
+		return this.addrs.addAll(addrs);
+	}
+
+	@Override
+	public boolean remove(Address addr)
+	{
+		return this.addrs.remove(addr);
+	}
+
+	@Override
+	public int size()
+	{
+		return this.addrs.size();
+	}
+
+	@Override
+	public Group union(Group other)
+	{
+		HashSet<Address> union = new HashSet<Address>(this.addrs);
+		union.addAll(other.getAll());
+		return new PartialReplicationGroup(union);
+	}
+
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder("GROUP:[");
+		for (Address a : addrs)
+		{
+			sb.append(a);
+			sb.append(" ");
+		}
+		sb.insert(sb.length() - 1, "]");
+
+		return sb.toString();
+	}
+
+	public boolean equals(Object obj)
+	{
+		return (obj instanceof PartialReplicationGroup)
+				&& (((PartialReplicationGroup) obj).size() == this.size())
+				&& (((PartialReplicationGroup) obj).addrs
+						.containsAll(this.addrs));
+	}
+
+	public int hashCode()
+	{
+		return addrs.hashCode();
+	}
+}
