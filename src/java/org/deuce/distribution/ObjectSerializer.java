a94637d65c28fa813bc2d2d0dd65521356491273
jaasilva <jaasilva@netcabo.pt>
2013-06-14 20:36:49 +0100

    minor changes.

--- src/java/org/deuce/distribution/ObjectSerializer.java
+++ src/java/org/deuce/distribution/ObjectSerializer.java
@@ -93,6 +93,7 @@ public abstract class ObjectSerializer
 			}
 			catch (NullPointerException e)
 			{
+				e.printStackTrace();
 				throw new GCPayloadException();
 			}
 			catch (ClassNotFoundException e)
