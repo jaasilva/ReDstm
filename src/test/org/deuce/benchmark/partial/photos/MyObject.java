c674634783c9a4cd9558f1d464ea54dced27da0d
Tiago Vale <tiagomarquesvale@gmail.com>
2013-06-26 14:55:18 +0100

    Removed @ExcludeTM from MyObject.

--- src/test/org/deuce/benchmark/partial/photos/MyObject.java
+++ src/test/org/deuce/benchmark/partial/photos/MyObject.java
@@ -2,9 +2,6 @@ package org.deuce.benchmark.partial.photos;
 
 import java.io.Serializable;
 
-import org.deuce.transform.ExcludeTM;
-
-@ExcludeTM
 public class MyObject implements Serializable
 {
 	private static final long serialVersionUID = 1L;
