3d9a0058c156bbb4deadca664541a1623f26ce73
jaasilva <jaasilva@netcabo.pt>
2013-06-26 09:42:00 +0100

    bug fix.

--- src/java/org/deuce/distribution/replication/partial/PartialReplicationSerializer.java
+++ src/java/org/deuce/distribution/replication/partial/PartialReplicationSerializer.java
@@ -65,7 +65,7 @@ public class PartialReplicationSerializer extends ObjectSerializer
 		}
 		else
 		{
-//			toPublish = oid.getGroup();
+			toPublish = oid.getGroup();
 //			if (toPublish.getAll().isEmpty())
 //			{ // choose group
 //				toPublish = TribuDSTM.publishObjectTo(obj);
