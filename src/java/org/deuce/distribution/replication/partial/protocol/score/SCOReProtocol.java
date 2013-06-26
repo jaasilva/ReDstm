977efffbf92ca33605bca5188fad91d3c220c71d
Tiago Vale <tiagomarquesvale@gmail.com>
2013-06-26 15:42:28 +0100

    Single-threaded read executor.

--- src/java/org/deuce/distribution/replication/partial/protocol/score/SCOReProtocol.java
+++ src/java/org/deuce/distribution/replication/partial/protocol/score/SCOReProtocol.java
@@ -72,8 +72,9 @@ public class SCOReProtocol extends PartialReplicationProtocol implements
 	private final Set<String> rejectTrxs = new HashSet<String>(); // accessed only by bottom threads
 
 	private static final int minReadThreads = 2;
-	private final Executor pool = Executors.newFixedThreadPool(Math.max(
-			TribuDSTM.getNumGroups() / 2, minReadThreads));
+//	private final Executor pool = Executors.newFixedThreadPool(Math.max(
+//			TribuDSTM.getNumGroups() / 2, minReadThreads));
+	private final Executor pool = Executors.newSingleThreadExecutor();
 
 	public static final ThreadLocal<Boolean> serializationContext = new ThreadLocal<Boolean>()
 	{ // false -> *not* read context; true -> read context
