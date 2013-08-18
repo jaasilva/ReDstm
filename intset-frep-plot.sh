f1986495a2e020fed22c364a548668183f67e0db
jaasilva <jaasilva@netcabo.pt>
2013-08-18 16:40:26 +0100

    bug in script.

--- intset-frep-plot.sh
+++ intset-frep-plot.sh
@@ -9,7 +9,7 @@ RUNS=10
 DATA="${DIR}/${BENCH}_${COMM}.data"
 
 rm -f $DATA
-echo '"Replicas"	"1 thread"	"2 threads"	"4 threads"	"6 threads"' >$DATA
+echo '"Replicas"	"1 thread"	"2 threads"	"4 threads"	"6 threads"	"8 threads"' >$DATA
 
 for replicas in 2 4 6 8 #10 20 30 40 50 60 70 80 90 100
 do
