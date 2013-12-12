c7e76dcad9e4cb6c34de8fa4aa15cb17152d5442
jaasilva <jaa.silva@campus.fct.unl.pt>
2013-12-12 17:07:54 +0000

    started python script for plotting results.

---
+++ scripts/plot/intset.py
@@ -0,0 +1,20 @@
+import sys
+
+def usage(argv):
+	print "Usage: %s <> <>" % argv[0]
+
+def plot_X():
+	pass
+
+def plot_Y():
+	pass
+
+def plot_Z():
+	pass
+
+def main(argv):
+	pass
+
+if __name__ == "__main__":
+	main(sys.argv)
+
