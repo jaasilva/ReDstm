573d7482d6f04ad86344170b043d8100ee4beaa1
jaasilva <jaa.silva@campus.fct.unl.pt>
2014-01-07 11:19:03 +0000

    experiment.

--- src/test/org/deuce/benchmark/intset/RedBTreeZ.java
+++ src/test/org/deuce/benchmark/intset/RedBTreeZ.java
@@ -714,14 +714,12 @@ public class RedBTreeZ implements IntSet
 	{
 		if (rand.nextInt(100) < partial_ops)
 		{ // partial operation
-//			if (rand.nextBoolean()) // 50%
-//			{
-				return partial_op_random(key);
-//			}
-//			else
-//			{
-//				return partial_op();
-//			}
+			Node node = lookup(key);
+			if (node != null)
+			{
+				node.v = rand.nextInt();
+			}
+			return node != null;
 		}
 		else
 		{ // full operation
@@ -781,7 +779,12 @@ public class RedBTreeZ implements IntSet
 	{
 		if (rand.nextInt(100) < partial_ops)
 		{ // partial operation
-			return partial_op_random(key);
+			Node node = lookup(key);
+			if (node != null)
+			{
+				node.v = rand.nextInt();
+			}
+			return node != null;
 		}
 		else
 		{ // full operation
