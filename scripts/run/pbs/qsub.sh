0ca52b3dfda172a0b2926c84b3dc201da2449028
jaasilva <jaa.silva@campus.fct.unl.pt>
2014-02-05 11:09:59 +0000

    changes to pbs files.

--- scripts/run/pbs/qsub.sh
+++ scripts/run/pbs/qsub.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ###############################################################################
 # <replicas>
 ###############################################################################
@@ -25,18 +25,14 @@ _jobId=""
 
 for _run in `seq 1 $_runs`
 do
-
-	_jobId=$(cat <<EOF | qsub -
-	
-	#!/bin/bash
-	#PBS -N ${_reps}-${_run}-${_groups}
+	_jobId=$(cat <<EOF | qsub - #!/bin/bash
 	#PBS -S /bin/bash
 	#PBS -A pdr_14
-	#PBS -l nodes=${_reps}:ppn=${_thrs}
+	#PBS -N ${_reps}-${_run}-${_groups}
 	#PBS -l walltime=00:05:00
+	#PBS -l nodes=${_reps}:ppn=${_thrs}
 	#PBS -q generic
-	#PBS -M jaa.silva@campus.fct.unl.pt
-	#PBS -m abe
+	#PBS -m abe -M jaa.silva@campus.fct.unl.pt
 	#PBS -j oe
 	#PBS -o ./logs/${_bench}_r${_reps}_t${_thrs}_w${_writes}_g${_groups}_${_run}_${_proto}.res
 	#PBS -W depend=afterany:$jobId
@@ -46,15 +42,20 @@ do
 	
 	cd $PBS_O_WORKDIR
 	
-	python ./scripts/run/pbs/create_task_file.py tasks $_reps ./scripts/run/pbs/intset-full_rep.sh $_bench $_thrs $_reps $_run $_writes $_pops
-	#python ./scripts/run/pbs/create_task_file.py tasks $_reps ./scripts/run/pbs/intset-par_rep.sh $_bench $_thrs $_reps $_run $_writes $_groups $_pops
+	python ./scripts/run/pbs/create_task_file.py \
+		tasks-${_reps}-${_run}-${_groups} $_reps \
+		./scripts/run/intset-full_rep.sh $_bench $_thrs \
+		$_reps $_run $_writes $_pops
+	#python ./scripts/run/pbs/create_task_file.py \
+	#	tasks-${_reps}-${_run}-${_groups} $_reps \
+	#	./scripts/run/intset-par_rep.sh $_bench $_thrs \
+	#	$_reps $_run $_writes $_groups $_pops
 	
-	mpipbsexec nop.sh
-	taskfarm tasks
+	mpipbsexec ./scripts/run/pbs/nop.sh
+	taskfarm tasks-${_reps}-${_run}-${_groups}
 	
 	echo "##### FINISHED!"
 	EOF)
-
 echo "> ${_jobId}"
 done
 
