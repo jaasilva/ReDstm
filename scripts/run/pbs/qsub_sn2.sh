6841206b722ad75978d970fd4cd0d99a5a4a5f2b
jaasilva <jaa.silva@campus.fct.unl.pt>
2014-02-06 08:55:39 +0000

    pbs.

--- scripts/run/pbs/qsub_sn2.sh
+++ scripts/run/pbs/qsub_sn2.sh
@@ -27,13 +27,14 @@ _jobId=$(cat <<EOF | qsub -
 #PBS -N rbtZ-full-n${_reps}
 #PBS -l select=${_reps}:ncpus=${_thrs}:mem=${_mem}:hw_model=${_model}
 #PBS -l place=scatter
-#PBS -q short6h
+#PBS -q normal
 #PBS -M jaa.silva@campus.fct.unl.pt
 #PBS -m abe
 #PBS -j oe
 #PBS -W depend=afterany:${_jobId}
 
-for _writes in 10 50; do
+for _writes in 10; do
+#for _writes in 50; do
 for _p_ops in 0 10 50 80 100; do	
 	for _run in `seq 1 $_runs`; do
 		
