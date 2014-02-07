658c0b5bbcb658b4afb2f6bea6bc339110c68190
jaasilva <jaa.silva@campus.fct.unl.pt>
2014-02-07 08:16:51 +0000

    join stuff.

--- src/java/org/deuce/distribution/replication/partial/protocol/score/SCOReProtocol.java
+++ src/java/org/deuce/distribution/replication/partial/protocol/score/SCOReProtocol.java
@@ -352,6 +352,7 @@ public class SCOReProtocol extends PartialReplicationProtocol implements
 			readReturn((ReadRet) obj, src);
 		}
 		processTx();
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> onDelivery.");
 	}
 
 	protected void processControlMessage(ControlMessage obj)
