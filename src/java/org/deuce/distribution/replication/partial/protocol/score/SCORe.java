421a8da286a18e974cd9e4901feee57b11a84e1f
jaasilva <jaa.silva@campus.fct.unl.pt>
2014-08-29 09:58:02 +0100

    stuff.

--- src/java/org/deuce/distribution/replication/partial/protocol/score/SCORe.java
+++ src/java/org/deuce/distribution/replication/partial/protocol/score/SCORe.java
@@ -455,6 +455,8 @@ public class SCORe extends PartialReplicationProtocol implements
 			stableQ.add(new Pair(txnID, finalSid));
 		}
 
+		if (txnID == null)
+			System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
 		boolean remove = pendQ.remove(new Pair(txnID, -1));
 		advanceCommitId();
 
