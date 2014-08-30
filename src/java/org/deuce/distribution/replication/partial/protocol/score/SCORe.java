2e57c6a30f4db7cb4357539bb535d9c5f92044fb
jaasilva <jaa.silva@campus.fct.unl.pt>
2014-08-30 20:01:35 +0100

    stuff.

--- src/java/org/deuce/distribution/replication/partial/protocol/score/SCORe.java
+++ src/java/org/deuce/distribution/replication/partial/protocol/score/SCORe.java
@@ -457,7 +457,7 @@ public class SCORe extends PartialReplicationProtocol implements
 		}
 
 		boolean remove = pendQ.remove(new Pair(txnID, -1));
-		if (!remove) System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
+		if (!remove && result) System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
 		advanceCommitId();
 
 		ContextState tx = (ContextState) receivedTxns.get(txnID);
