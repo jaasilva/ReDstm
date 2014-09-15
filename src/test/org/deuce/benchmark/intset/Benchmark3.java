4d26765816eb6f61b69a9f7a656f2dc8acbe0cd3
jaasilva <jaa.silva@campus.fct.unl.pt>
2014-09-15 12:54:53 +0100

    another benchmark.

---
+++ src/test/org/deuce/benchmark/intset/Benchmark3.java
@@ -0,0 +1,216 @@
+package org.deuce.benchmark.intset;
+
+import java.util.Random;
+
+import org.deuce.Atomic;
+import org.deuce.distribution.Defaults;
+import org.deuce.distribution.replication.Bootstrap;
+
+/**
+ * @author Pascal Felber
+ * @since 0.1
+ */
+public class Benchmark3 implements org.deuce.benchmark.Benchmark
+{
+	@Bootstrap(id = 3)
+	public IntSet m_set;
+	int m_range = 1 << 16;
+	int m_rate = 20;
+	int initial = 32768; // 2^15
+
+	@Atomic
+	private void setRange(int x)
+	{
+		m_range = x;
+	}
+
+	@Atomic
+	private void setWrites(int x)
+	{
+		m_rate = x;
+	}
+
+	@Atomic
+	private void setInitial(int x)
+	{
+		initial = x;
+	}
+
+	@Override
+	public void init(String[] args)
+	{
+		boolean error = false;
+
+		if (args.length == 0)
+			error = true;
+		for (int i = 1; i < args.length && !error; i++)
+		{
+			if (args[i].equals("-i"))
+			{
+				if (++i < args.length)
+				{
+					initial = Integer.parseInt(args[i]);
+					setInitial(initial);
+					System.setProperty(Defaults._RBTREE_INITIAL, "" + initial);
+				}
+				else
+					error = true;
+			}
+			else if (args[i].equals("-r"))
+			{
+				if (++i < args.length)
+				{
+					m_range = Integer.parseInt(args[i]);
+					setRange(m_range);
+				}
+				else
+					error = true;
+			}
+			else if (args[i].equals("-w"))
+			{
+				if (++i < args.length)
+				{
+					m_rate = Integer.parseInt(args[i]);
+					setWrites(m_rate);
+				}
+				else
+					error = true;
+			}
+			else if (args[i].equals("-po"))
+			{
+				if (++i < args.length)
+				{
+					int partial_ops = Integer.parseInt(args[i]);
+					System.setProperty(Defaults._RBTREE_PARTIAL_OPS, ""
+							+ partial_ops);
+				}
+				else
+					error = true;
+			}
+			else
+				error = true;
+		}
+		if (Integer.getInteger("tribu.site") == 1)
+		{
+			error = initSet(args[0]);
+
+			if (error)
+			{
+				System.out
+						.println("Benchmark arguments: (LinkedList|SkipList|RBTree) [-i initial-size] [-r range] [-w write-rate]");
+				System.exit(1);
+			}
+
+			System.err.println("Set created.");
+
+			long start = System.nanoTime();
+			initializeSet(new Random(), initial);
+			long end = System.nanoTime();
+			System.err.println("Set populated: " + (end - start) / 1000000000.0
+					+ " s");
+
+			System.out.println("Initial size  = " + initial);
+			System.out.println("Range         = " + m_range);
+			System.out.println("Write rate    = " + m_rate + "%");
+			System.out.println();
+		}
+	}
+
+	@Atomic
+	private boolean initSet(String type)
+	{
+		if (type.equals("LinkedList"))
+		{
+			if (m_set == null)
+			{
+				m_set = new IntSetLinkedList();
+			}
+		}
+		else if (type.equals("RedBTree"))
+		{ // supposed good red black tree
+			if (m_set == null)
+			{
+				m_set = new RedBTree();
+			}
+		}
+		else if (type.equals("RedBTreeX"))
+		{ // memory benchmark
+			if (m_set == null)
+			{
+				m_set = new RedBTreeX();
+			}
+		}
+		else if (type.equals("RedBTreeZ"))
+		{ // partial replication benchmark
+			if (m_set == null)
+			{
+				m_set = new RedBTreeZ();
+			}
+		}
+		else if (type.equals("SkipList"))
+		{
+			if (m_set == null)
+			{
+				m_set = new IntSetSkipList();
+			}
+		}
+		else
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public void initializeSet(Random random, int initial)
+	{
+		int chunkSize = initial / 8;
+		for (int i = 0; i < initial; i += chunkSize)
+		{
+			System.err.println("[" + i + "-" + (i + chunkSize) + "]");
+			addToSet(random, chunkSize);
+		}
+	}
+
+	@Atomic
+	public void addToSet(Random random, int chunkSize)
+	{
+		for (int i = 0; i < chunkSize; i++)
+		{
+			while (!m_set.initAdd(random.nextInt(m_range)))
+				;
+		}
+	}
+
+	@Override
+	public org.deuce.benchmark.BenchmarkThread createThread(int i, int nb)
+	{
+		int id = Integer.getInteger("tribu.site") - 1;
+		int reps = Integer.getInteger("tribu.replicas");
+		int size = m_range / reps;
+		int min = id * size;
+		int max = min + size;
+		
+		return new BenchmarkThread3(m_set, m_range, m_rate, max, min);
+	}
+
+	@Override
+	public String getStats(org.deuce.benchmark.BenchmarkThread[] threads)
+	{
+		int add = 0;
+		int remove = 0;
+		int contains = 0;
+		for (int i = 0; i < threads.length; i++)
+		{
+			add += ((BenchmarkThread3) threads[i]).m_nb_add;
+			remove += ((BenchmarkThread3) threads[i]).m_nb_remove;
+			contains += ((BenchmarkThread3) threads[i]).m_nb_contains;
+		}
+		return "A=" + add + ", R=" + remove + ", C=" + contains;
+	}
+
+	@Override
+	public boolean validate()
+	{
+		return m_set.validate();
+	}
+}
---
+++ src/test/org/deuce/benchmark/intset/BenchmarkThread3.java
@@ -0,0 +1,88 @@
+package org.deuce.benchmark.intset;
+
+import java.util.Random;
+
+import org.deuce.Atomic;
+
+/**
+ * @author Pascal Felber
+ * @since 0.1
+ */
+public class BenchmarkThread3 extends org.deuce.benchmark.BenchmarkThread
+{
+	final private IntSet m_set;
+	final private int m_range;
+	int m_nb_add;
+	int m_nb_remove;
+	int m_nb_contains;
+	final private int m_rate;
+	boolean m_write;
+	final private Random m_random;
+	final private int max, min;
+
+	public BenchmarkThread3(IntSet set, int range, int rate, int max, int min)
+	{
+		m_set = set;
+		m_range = range;
+		m_nb_add = m_nb_remove = m_nb_contains = 0;
+		m_rate = rate;
+		m_write = true;
+		m_random = new Random();
+		this.max = max;
+		this.min = min;
+	}
+
+	protected void step(int phase)
+	{
+		int i = m_random.nextInt(100);
+		if (i < m_rate)
+		{
+			if (m_write)
+			{
+				addTen();
+				if (phase == Benchmark.TEST_PHASE)
+					m_nb_add++;
+				m_write = false;
+			}
+			else
+			{
+				removeTen();
+				if (phase == Benchmark.TEST_PHASE)
+					m_nb_remove++;
+				m_write = true;
+			}
+		}
+		else
+		{
+			containsTen();
+			if (phase == Benchmark.TEST_PHASE)
+				m_nb_contains++;
+		}
+	}
+
+	@Atomic
+	private void addTen()
+	{
+		for (int j = 0; j < 10; ++j)
+			m_set.add(m_random.nextInt((max - min)) + min);
+	}
+
+	@Atomic
+	private void removeTen()
+	{
+		for (int j = 0; j < 10; ++j)
+			m_set.remove(m_random.nextInt((max - min)) + min);
+	}
+
+	@Atomic
+	private void containsTen()
+	{
+		for (int j = 0; j < 10; ++j)
+			m_set.contains(m_random.nextInt((max - min)) + min);
+	}
+
+	public String getStats()
+	{
+		return "A=" + m_nb_add + ", R=" + m_nb_remove + ", C=" + m_nb_contains;
+	}
+}
